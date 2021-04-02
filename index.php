a05680e062532d4fd3890a2afda42f40490cb087
Benyamin Limanto <blbenyamin9@gmail.com>
2021-04-03 00:06:45 +0700

    Add some asthetic, adding nasional kontan

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <html>
     <head> 
-        <title>HTML News Agregator</title>
+        <title>Indonesian HTML News Viewer</title>
     </head>
     <body>
+    <center>
+    <h1 style="margin-bottom: 0px;">Berita Indonesia</h1>
+    <h5 style="margin-top: 0px;">dalam HTML</h5>
+    </center>
+    <hr/>
     <?php
         // Load the RSS of Kontan
         $kontan = [
-            0 => "http://rss.kontan.co.id/news/investasi" 
+            'investasi' => "http://rss.kontan.co.id/news/investasi",
+            'nasional'  => "http://rss.kontan.co.id/news/nasional"
         ];
 
         foreach($kontan as $n => $u) {
@@ -28,12 +34,17 @@
                     $result .= $file->fgets();
                 }
             }
+            $time = DateTime::createFromFormat("U", $file->getMTime());
+            $time->setTimeZone(new DateTimeZone("Asia/Jakarta"));
             // Read the XML 
             $file = null; // close file
             $xml = new XMLReader();
             $xml->XML($result);
             $ttl = false;
+            echo "<h3 style='margin-bottom:0px;'>Kontan ".ucfirst($n)."</h3>";
+            echo "<h6 style='margin-top:2px;margin-bottom:1px;'><i>Last Update : ".$time->format("d-M-Y H:i T")."</i></h6>";
             $i = 0;
+            echo "<ol>";
             $rssData = []; $temp = [];
             while ($xml->read()) {
                 if (!$ttl) {
@@ -57,10 +68,11 @@
                 if ($i == 2) {
                     $rssData[] = $temp;
                     $url = "news/index.php?url=".urlencode("{$temp[1]}?page=all").".html";
-                    echo "<a href='$url'>{$temp[0]}</a><br/>";
+                    echo "<li><a href='$url'>{$temp[0]}</a></li>";
                     $i = 0; $temp = [];
                 }
             }
+            echo "</ol>";
         }
     ?>
     </body>
